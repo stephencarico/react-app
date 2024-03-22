b99a777917ede3cd49f7b1571b612af970bfbecc
Stephen Carico <32233894+stephencarico@users.noreply.github.com>
2024-03-21 21:20:55 -0600

    feat: create ListGroup

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,20 @@
+function ListGroup() {
+  let items = ["Paris", "London", "New York", "San Francisco", "Denver"];
+  items = [];
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item} className="list-group-item">
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
