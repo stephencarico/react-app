5f873c509a28740b3f3d6c9976ff2bdf7966c02b
Stephen Carico <32233894+stephencarico@users.noreply.github.com>
2024-03-23 15:48:01 -0600

    feat: dismiss Alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
+
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(true);
+
   return (
     <div>
-      <Button onClick={() => console.log("Clicked")} color="link">
-        Hello World!
-      </Button>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>
+          Here is my alert!
+        </Alert>
+      )}
+      <Button onClick={() => setAlertVisibility(true)}>Hello World!</Button>
     </div>
   );
 }
