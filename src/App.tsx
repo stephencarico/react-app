58eedbe1263fbfacb604a727128fb5348eb41d17
Stephen Carico <32233894+stephencarico@users.noreply.github.com>
2024-03-26 10:14:16 -0600

    feat: create Form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div>
-      <ExpandableText maxChars={25}>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum a
-        provident dicta est officia cum mollitia dolores labore illum? Nemo,
-        deleniti earum iusto error omnis sunt? Ipsam iste itaque ex culpa odio
-        ab sed molestias, ratione deserunt placeat quaerat illum ipsum fugit
-        quos vitae quia perspiciatis nobis laboriosam eos, aspernatur unde alias
-        amet! Est voluptate, ullam, quia quasi optio amet enim, rerum magni
-        accusantium pariatur tempora neque ad laboriosam quod adipisci
-        reiciendis similique natus praesentium deleniti error consequatur rem
-        nobis! Velit at et ex similique quibusdam! Nemo sunt, eligendi quasi,
-        possimus molestiae laudantium accusantium iusto ullam quia aperiam
-        aspernatur a!
-      </ExpandableText>
+      <Form></Form>
     </div>
   );
 }
