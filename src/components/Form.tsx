e39052b6330b8777f749184cb31bd5719e85a965
Stephen Carico <32233894+stephencarico@users.noreply.github.com>
2024-03-27 11:41:55 -0600

    feat: update Form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,10 +3,11 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 const schema = z.object({
-  name: z.string().min(3, { message: "Name must be at least 3 characters." }),
-  age: z
-    .number({ invalid_type_error: "Age field is required." })
-    .min(18, { message: "Age must be at least 18" }),
+  description: z
+    .string()
+    .min(3, { message: "Description should be at least 3 characters." }),
+  amount: z.number({ invalid_type_error: "Amount is required." }).positive(),
+  category: z.string().min(1, { message: "Category is required." }),
 });
 
 type FormData = z.infer<typeof schema>;
@@ -24,29 +25,53 @@ const Form = () => {
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
-          Name
+          Description
         </label>
         <input
-          {...register("name")}
-          id="name"
+          {...register("description")}
+          id="description"
           type="text"
           className="form-control"
         />
-        {errors.name && <p className="text-danger">{errors.name.message}</p>}
+        {errors.description && (
+          <p className="text-danger">{errors.description.message}</p>
+        )}
       </div>
       <div className="mb-3">
-        <label htmlFor="age" className="form-label">
-          Age
+        <label htmlFor="amount" className="form-label">
+          Amount
         </label>
         <input
-          {...register("age", { valueAsNumber: true })}
-          id="age"
+          {...register("amount", { valueAsNumber: true })}
+          id="amount"
           type="number"
           className="form-control"
         />
-        {errors.age && <p className="text-danger">{errors.age.message}</p>}
+        {errors.amount && (
+          <p className="text-danger">{errors.amount.message}</p>
+        )}
       </div>
-      <button disabled={!isValid} className="btn btn-primary" type="submit">
+      <div className="mb-3">
+        <label htmlFor="category" className="form-label">
+          Category
+        </label>
+        <select
+          {...register("category")}
+          id="category"
+          className="form-select"
+          aria-label="Category select field"
+          defaultValue={""}
+        >
+          <option></option>
+          <option value="Groceries">Groceries</option>
+          <option value="Utilities">Utilities</option>
+          <option value="Entertainment">Entertainment</option>
+        </select>
+        {errors.category && (
+          <p className="text-danger">{errors.category.message}</p>
+        )}
+      </div>
+      <button className="btn btn-primary" type="submit">
         Submit
       </button>
     </form>
