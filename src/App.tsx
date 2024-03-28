ce475ee9f32215f2e26a7000a5fa7f164f3f4bdf
Stephen Carico <32233894+stephencarico@users.noreply.github.com>
2024-03-27 23:51:37 -0600

    feat: fetch users with axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-import ExpenseTracker from "./expense-tracker/ExpenseTracker";
+interface User {
+  id: number;
+  name: string;
+}
 
 function App() {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data));
+  }, []);
+
   return (
-    <div>
-      <ExpenseTracker />
-    </div>
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
   );
 }
 
